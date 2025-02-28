8d611ecbe7c37b1da3bcb96576b1a2305b36dae8
NamiK77 <namigr78@gmail.com>
2025-02-28 16:03:33 +0545

    Bookscreen updating the the design

--- Screens/BookScreen.js
+++ Screens/BookScreen.js
@@ -134,7 +134,12 @@ const BookScreen = () => {
         </View>
       </Pressable>
 
-      
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={venues}
+        renderItem={({ item }) => <VenueCard item={item} />}
+        contentContainerStyle={{ paddingBottom: 20 }}
+      />
     </SafeAreaView>
   );
 };
